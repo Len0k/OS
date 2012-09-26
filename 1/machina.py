aea9c63b31bbfa28bb5e741da7178878d06fa0d6
Len0k <len0k.matmex@gmail.com>
2012-09-26 21:09:58 +0600

    Ready; made together with Irina Burdova

--- 1/machina.py
+++ 1/machina.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python
 import sys
 
-hash = {'00': ["", 0, 0, 0], '11': [0, 1, 1, 0], '15': [1, 1 ,1 ,0],
-	'02': ["", 2, 0, 0], '21': [0, 1, 2, 0], '25': [1, 1, 2, 0],
-	'31': [0, 1, 3, 0], 'FE': ["", 4, 15, 1], 'F0': ["", 4, 15, 0],
-		'F1': ["", 4, 15, 0], 'F4': ["", 4, 15, 1],
-		'F5': ["", 4, 15, 0], 'FF': ["", 4, 15, 0] }
+com_table = {'00': [1, 0, 0, 0], '11': [0, 1, 1, 0], '15': [1, 1 ,1 ,0],
+	'02': [0, 2, 0, 0], '21': [0, 1, 2, 0], '25': [1, 1, 2, 0],
+	'31': [0, 1, 3, 0], 'FE': [-1, 4, 15, 1], 'F0': [-1, 4, 15, 0],
+		'F1': [1, 4, 15, 0], 'F4': [1, 4, 15, 1],
+		'F5': [1, 4, 15, 0], 'FF': [1, 4, 15, 0] }
 	
 def main():
+	global RON, RVV, zapp, zam1, vzap1, zam2, chist, pusk, vib, com_table
+	if (len(sys.argv)<2):
+		print "Usage: program filename [zapp|zam1|vzap1|zam2|chist|pusk|vib|op|pereh]"
 	proga = open(sys.argv[1]).read().split("\n")
+	if (len(sys.argv) > 2):
+		broken = sys.argv[2]
+		if broken == 'op':
+			for i in com_table.keys():
+				com_table[i][2] = 0
+		if broken == 'pereh':
+			for i in com_table.keys():
+				com_table[i][3] = 0
 	for i in xrange(len(proga)):
-		(ip, com) = proga[i].split(":")
+		ip, com = proga[i].split(":")
 		ip = int(ip)
-		(cop, arg) = com.split()
+		cop, arg = com.split()
 		arg = int(arg)
 		proga[i] = [ip, cop, arg]
+	pusk = 1
 	IP = 0
 	IR = 0
 	global RON
-	while (1):
-		COP = proga[IP][1]
+	while 1:
+		com_table['F0'][3]  = com_table['F1'][3] = 1 - RON
+		global COP
+		COP = proga[IP][1].upper()
 		ADDR = proga[IP][2]
 		dekkom(COP)
-		IA = t1 = IP + IR
+		setConstants()
+		IA = t1 = ADDR + IR
 		t0 = proga[t1][2]
 		t2 = RVV
 		t1 = m(t0, t1, t2, vib)
@@ -30,64 +45,74 @@ def main():
 		t0 = alu (t0, t1)
 		# IR
 		t = m(t0, 0, chist)
-		if (zam2 == 1):
+		if zam2 == 1:
 			IR = t
 		# RON
-		if (zam1 == 1):
+		if zam1 == 1:
 			RON = t0
 		# memory
-		if (zapp == 1):
+		if zapp == 1:
 			proga[IP+IA][2] = t0
 		# next command
-		t = m(IP+1, IA, hash[COP][3])
-		if (pusk == 1):
+		t = m(IP+1, IA, com_table[COP][3])
+		if pusk == 1:
 			IP = t
 		else:
 			break
+	#print finish state
+	print "Instruction pointer: " + str(IP)
+	print "General-purpose register: " + str(RON)
+	print "Index register: " + str(IR)
+	#print memory
+	print "Memory:"
+	for i in xrange(len(proga)):
+		print i, ':', proga[i][2]
 
 def dekkom(com):
-	global RON
-	hash[com][3] = 1 - RON
-	p = hash[com][1]
-	if (p == 0):
-		zapp = 1
-	if (p == 1):
-		zam1 = 1
-	if (p == 3):
-		vzap1 = 1
-	if (p != 3):
-		zam2 = 1
-	if not (p == 2 or p ==3):
-		chist = 1
-	if (com != 'FF'):
-		pusk = 1
-	vib = hash[com][0]
+	global RON, zapp, zam1, vzap1, zam2, chist, pusk, vib, com_table
+	p = com_table[com][1]
+	zapp = 1 if p == 0 else 0
+	zam1 = 1 if p == 1 else 0
+	vzap1 = 1 if p == 3 else 0
+	zam2 = 1 if p != 3 else 0
+	chist = 1 if p ==2 or p ==3 else 0
+	pusk = 0 if com == 'FF' else 1
+	vib = com_table[com][0]
 
 def alu(arg0, arg1):
-	global COP
-	op = hash[COP][2]
+	global COP, com_table, RON
+	op = com_table[COP][2]
 	#switch (op):
-	if (op == 0):
+	if op == 0:
 		return arg0
-	if (op == 1):
+	if op == 1:
 		return arg1
-	if (op == 2):
+	if op == 2:
 		return arg0 + arg1
-	if (op == 3):
+	if op == 3:
 		return arg1 - arg0
-	if (op == 15):
-		return -1
+	if op == 15:
+		return RON
 	return -1
 
-def ukkom(adrcom):
-	if (pusk == 1):
-		ip = adrcom
-	return ip
 def m(*args):
 	return args[args[-1]]
+
+def setConstants():
+	global zapp, zam1, vzap1, zam2, chist, pusk, vib, com_table
+	num_hash = {'zapp' : 0, 'zam1' : 1, 'vzap1' : 2,
+		'zam2': 3, 'chist' : 4, 'pusk' : 5, 'vib' : 6}
+	if (len(sys.argv) > 2):
+		broken = sys.argv[2]
+		if broken in ('zapp', 'zam1', 'vzap1', 'zam2', 'chist', 'pusk', 'vib'):
+			(zapp, zam1, vzap1, zam2, chist, pusk, vib)[num_hash[broken]] = 0
+
+
 	
-(p, zapp, zapp1, vzap1, zam1, op, pusk, chist, RON, IP, ADDR, IR) = (0,0,0,0,0,0,0,0,0,0, 0, 0)
+(p, zapp, zapp1, vzap1, zam1, op, chist, RON, IP, ADDR, IR) = (0,0,0,0,0,0,0,0,0, 0, 0)
 RVV = 0
 vib = 0
 COP = ""
+zam2 = 0
+
 main()
