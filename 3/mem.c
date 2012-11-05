9946ad553cfef03fa55482a2e4400b2441dc553d
Elena <len0k.matmex@gmail.com>
2012-11-05 20:29:52 +0400

    It works. Now need to add parsing memory and deallocation

--- 3/mem.c
+++ 3/mem.c
@@ -6,11 +6,22 @@ EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
 	EFI_MEMORY_DESCRIPTOR * md;
 	UINTN md_size, md_key;
 	UINT32 md_version;
-	EFI_STATUS res = EFI_SUCCESS;
+	EFI_STATUS res;
 
 	InitializeLib(image, systab);
 	res = uefi_call_wrapper(systab->BootServices->GetMemoryMap, 5, &N, NULL, NULL, NULL, NULL);
-	Print(L"Size requared: %d\n\r", N);
-
+	Print(L"Size required: %d\n\r", N);
+	res = uefi_call_wrapper(systab->BootServices->AllocatePool, 3, EfiLoaderData, N, &md);
+	if (res != EFI_SUCCESS){
+		Print (L"Failed to allocate memory for memory map!\r\n");
+		return EFI_SUCCESS;
+	}
+	res = uefi_call_wrapper(systab->BootServices->GetMemoryMap, 5, &N, md, &md_key, &md_size, &md_version);
+	if (res != EFI_SUCCESS){
+		Print (L"Error occured during GetMemoryMap: %d\r\n", res);
+		Print (L"Size required: %d", N);
+		return EFI_SUCCESS;
+	}
+	Print (L"Ok\r\n");
 	return EFI_SUCCESS;
 }
